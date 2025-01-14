cd57a7226705e8c6b35fa57a6326158a5de529f9
lucas-de-souza-pereira <lucas.de-souza-pereira@laplateforme.io>
2025-01-14 13:05:42 +0100

    feat : added error handling system
    add system for check operator error and replace , by .

--- main.py
+++ main.py
@@ -1,67 +1,85 @@
 
-def calcul (num1,operator,num2):
+def calcul (number_1,operator,number_2):
     if operator == "+":
-        return num1 + num2
+        return number_1 + number_2
     elif operator == "-":
-        return num1 - num2
+        return number_1 - number_2
     elif operator == "*":
-        return num1 * num2
+        return number_1 * number_2
     elif operator == "/":
-        if num2 != 0:
-            return num1 / num2
+        if number_2 != 0:
+            return number_1 / number_2
         else: 
             return "division par 0 impossible"
     elif operator == "%":
-        if num2 != 0:
-            return num1 % num2
+        if number_2 != 0:
+            return number_1 % number_2
         else:  
             return "modulo par 0"
+    elif operator == "**":
+        return number_1 ** number_2
+    elif operator == "//":
+        if number_2 != 0:
+            return number_1 // number_2
+        else: 
+            return "division par 0 impossible"
 
-
-
-def demande_num1():
+def demande_number_1():
     try:
-        num1 = input("nombre 1 : ")
-        if "." or "," in num1:
-            return float(num1.replace(",","."))
+        number_1 = input("nombre 1 : ")
+        if "." in number_1 or "," in number_1 :
+            return float(number_1.replace(",","."))
         else:
-            return int(num1)
+            return int(number_1)
     except ValueError:
-        print("Erreur de saisie, rentrez un ou plusieurs chiffres")
-        return demande_num1()
-
+        print("\nErreur de saisie, rentrez un ou plusieurs chiffres\n")
+        return demande_number_1()
 
 def demande_operator():
     
         operator = input("operateur : ")
-        return operator
 
-def demande_num2():
-    try:
-        num2 = input("nombre 2 : ")
-        if "." or "," in num2:
-            return float(num2.replace(",","."))
+        if operator == "+" or operator == "-" or operator == "*" or operator == "/" \
+            or operator == "%" or operator == "**":
+            return operator
         else:
-            return int(num2)
+            ValueError
+            print("\nErreur de saisie, rentrez un opérateur valide")
+            question = input("\nVoulez-vous afficher la liste des opérateurs ? \
+                             \nRépondez par O ou N : ")
+            if question == "O":
+                print("\nVoici les opérateurs valides : \nAddition : + \nSoustraction : - \
+                      \nMultiplication : * \nDivision : / \nModulo : % \nPuissance : **  \nDivision entière : // \n")
+                return demande_operator()
+            else:
+                return demande_operator()
+
+
+def demande_number_2():
+    try:
+        number_2 = input("nombre 2 : ")
+        if "." in number_2 or "," in number_2 :
+            return float(number_2.replace(",","."))
+        else:
+            return int(number_2)
     except ValueError:
         print("Erreur de saisie, rentrez un ou plusieurs chiffres")
-        return demande_num1()
+        return demande_number_2()
 
 def demande():
-    num1 = demande_num1()
+    number_1 = demande_number_1()
     operator = demande_operator()
-    num2 = demande_num2()  
-    return num1,operator,num2
-
+    number_2 = demande_number_2()  
+    return number_1,operator,number_2
 
 
 def main():
 
-    num1,operator,num2 = demande()
+    number_1,operator,number_2 = demande()
 
-    resultat = calcul(num1,operator,num2)
+    resultat = calcul(number_1,operator,number_2)
 
-    print(f"{num1} {operator} {num2} = {resultat}")
+    print(f"\n{number_1} {operator} {number_2} = {resultat}\n")
 
 
 if __name__ == "__main__":
